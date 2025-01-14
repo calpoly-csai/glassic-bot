39b6b961efb6ec8a7405063759250689861b2cff
MrEminent42 <github@leohorwitz.com>
2025-01-14 12:06:20 -0800

    change discord message

--- src/discord/commands/Sync.Discord.ts
+++ src/discord/commands/Sync.Discord.ts
@@ -2,6 +2,7 @@ import { ChatInputCommandInteraction } from "discord.js";
 import DiscordClient from "../classes/DiscordClient";
 import SubCommand from "../classes/SubCommand";
 import getDiscordEventsJob from "../../jobs/syncDiscordEventsJob";
+import { CONFIG } from "../..";
 
 export default class SyncDiscord extends SubCommand {
     constructor(client: DiscordClient) {
@@ -11,7 +12,7 @@ export default class SyncDiscord extends SubCommand {
     }
 
     Execute(interaction: ChatInputCommandInteraction) {
-        interaction.reply({ content: "Syncing events from Notion to Discord. See logs in the bot logs channel.", ephemeral: true })
+        interaction.reply({ content: `Syncing events from Notion to GCal. See logs in <@${CONFIG.discord.logs.channel_id}>.`, ephemeral: true })
         getDiscordEventsJob(this.client)();
     }
 }
